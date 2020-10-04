88a8b8827257029a7e407d87f405a530dec78816
Brian Marick <marick@exampler.com>
2020-10-04 16:08:30 -0500

    Improve get tests.

--- test/impl/get_test.exs
+++ test/impl/get_test.exs
@@ -18,13 +18,34 @@ defmodule Impl.GetTest do
     end
 
     test "phoenix format" do
-      ok = %{params: %{age: 1, date: "2011-02-03"}}
+      ok = %{params: %{age: 1,
+                       date: "2011-02-03",
+                       nested: %{a: 3},
+                       list: [1, 2, 3]}}
       
       Build.start(__MODULE__, format: :phoenix)
       Build.category(__MODULE__, :valid, %{ok: ok})
 
       Get.get_params(__MODULE__, :ok)
-      |> assert_fields(%{"age" => "1", "date" => "2011-02-03"})
+      |> assert_fields(%{
+            "age" => "1",
+            "date" => "2011-02-03",
+            "nested" => %{"a" => "3"},
+            "list" => ["1", "2", "3"]})
+    end
+    
+    test "raw format" do
+      raw = %{age: 1,
+              date: "2011-02-03",
+              nested: %{a: 3},
+              list: [1, 2, 3]}
+      ok = %{params: raw}
+      
+      Build.start(__MODULE__, format: :raw)
+      Build.category(__MODULE__, :valid, %{ok: ok})
+
+      Get.get_params(__MODULE__, :ok)
+      |> assert_fields(raw)
     end
   end
 end 
