147f072fb3fd091358ce66dc063ace3f5a99b786
Brian Marick <marick@exampler.com>
2020-10-07 14:44:01 -0500

    Assert categories can be repeated.

--- test/api/category_test.exs
+++ test/api/category_test.exs
@@ -1,10 +1,9 @@
-defmodule Api.LikeTest do
+defmodule Api.CategoryTest do
   use TransformerTestSupport.Case
   use TransformerTestSupport.Impl.Predefines
 
   defmodule Variant do
-    def adjust_top_level(test_data),
-      do: Map.put(test_data, :adjusted, true)
+    def adjust_top_level(test_data), do: test_data
   end
 
   @minimal_start [module_under_test: Anything, variant: Variant]
@@ -13,40 +12,11 @@ defmodule Api.LikeTest do
     start(@minimal_start)
   end
 
-  test "like" do 
-    category(:valid, ok: [                    params(a: 1, b: 2)])
-    category(:invalid, similar: [params_like(:ok, except: [b: 4])])
+  test "you can repeat a category" do 
+    category(:valid, ok:    [params(a: 1,  b: 2)])
+    category(:valid, other: [params(a: 11, b: 22)])
 
-    assert example(:similar).params ==             %{a: 1, b: 4}
-  end
-
-  test "using like to refer to values within the same category" do
-    category(:valid, [
-          ok: [params: %{a: 1, b: 2}],
-          similar: [params_like(:ok, except: [b: 4])]
-        ])
-
-    assert example(:similar).params == %{a: 1, b: 4}
-  end
-
-  test "multiple categories" do
-    category(:valid, [
-          ok: [params: %{a: 1, b: 2}],
-          similar: [params_like(:ok, except: [b: 4])]
-        ])
-    category(:invalid, [
-          different: [params_like(:ok, except: [c: 383])]
-        ])
-
-    assert example(:ok).params ==        %{a: 1, b: 2}
-    assert example(:similar).params ==   %{a: 1, b: 4}
-    assert example(:different).params == %{a: 1, b: 2, c: 383}
-  end
-
-  test "like can copy everything" do 
-    category(:valid, ok: [params(a: 1, b: 2)])
-    category(:invalid, similar: [params_like(:ok)])
-
-    assert example(:similar).params == example(:similar).params
+    assert get_params(:ok) ==    %{a: 1,  b: 2}
+    assert get_params(:other) == %{a: 11, b: 22}
   end
 end  
