96c58cf34669244add5df5b50b79845b230f4f54
Brian Marick <marick@exampler.com>
2020-10-13 15:53:37 -0500

    Disable tests that will soon change

--- test/variants/changeset/category_adjustment_test.exs
+++ test/variants/changeset/category_adjustment_test.exs
@@ -5,34 +5,38 @@ defmodule Variants.EctoClassic.CategoryAdjustmentTest do
 #  import FlowAssertions.Define.Tabular
 
   describe "modifications" do
+    @tag :skip
     test "prepend to existing changeset assertions" do
-      example = %{changeset: [changes: [field: "value"]]}
-      actual = EctoClassic.run_example_hook(example, :valid)
+      # example = %{changeset: [changes: [field: "value"]]}
+      # actual = EctoClassic.run_example_hook(example, :valid)
 
-      assert actual == %{changeset: [:valid, changes: [field: "value"]]}
+      # assert actual == %{changeset: [:valid, changes: [field: "value"]]}
     end
 
+    @tag :skip
     test "create a validity assertion" do
-      example = %{}
-      actual = EctoClassic.run_example_hook(example, :invalid)
+      # example = %{}
+      # actual = EctoClassic.run_example_hook(example, :invalid)
 
-      assert actual == %{changeset: [:invalid]}
+      # assert actual == %{changeset: [:invalid]}
     end
 
+    @tag :skip
     test "a map is acceptable" do 
-      example = %{changeset: %{changes: [field: "value"]}}
-      actual = EctoClassic.run_example_hook(example, :valid)
+      # example = %{changeset: %{changes: [field: "value"]}}
+      # actual = EctoClassic.run_example_hook(example, :valid)
 
-      assert actual == %{changeset: [:valid, changes: [field: "value"]]}
+      # assert actual == %{changeset: [:valid, changes: [field: "value"]]}
     end
   end
 
+  @tag :skip
   test "bad categories are flagged" do
-    assertion_fails(
-      ~r/only allows these categories/,
-      [left: :broken],
-      fn -> 
-        EctoClassic.run_example_hook(%{}, :broken)
-      end)
+    # assertion_fails(
+    #   ~r/only allows these categories/,
+    #   [left: :broken],
+    #   fn -> 
+    #     EctoClassic.run_example_hook(%{}, :broken)
+    #   end)
   end
 end
