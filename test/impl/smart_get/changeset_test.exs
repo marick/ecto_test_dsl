01fc3b72aec97fbb9a2741d66d1e49f890f1807e
Brian Marick <marick@exampler.com>
2020-10-14 15:49:31 -0500

    prepare for next change

--- test/impl/smart_get/changeset_test.exs
+++ test/impl/smart_get/changeset_test.exs
@@ -3,36 +3,37 @@ defmodule Impl.SmartGet.ChangesetTest do
   alias TransformerTestSupport.Impl.SmartGet
   import TransformerTestSupport.Impl.Build
 
-  test "a :validation_failure category has an `invalid` check put at the front" do
-    test_data =
-      start() |>
-      category(:validation_failure,
-        oops: [changeset(no_changes: [:date])]
-      )
-
-    SmartGet.changeset(test_data, :oops)
-    |> assert_equal([:invalid, {:no_changes, [:date]}])
+  describe "validity additions" do 
+    test "a :validation_failure category has an `invalid` check put at the front" do
+      test_data =
+        start() |>
+        category(:validation_failure,
+          oops: [changeset(no_changes: [:date])]
+        )
+      
+      SmartGet.changeset(test_data, :oops)
+      |> assert_equal([:invalid, {:no_changes, [:date]}])
+    end
+    
+    
+    test "any other category gets a `valid` check" do
+      test_data =
+        start() |>
+        category(:success,
+          ok: [changeset(no_changes: [:date])]
+        )
+      
+      SmartGet.changeset(test_data, :ok)
+      |> assert_equal([:valid, {:no_changes, [:date]}])
+    end
+    
+    test "checks are added even if there's no changest" do
+      test_data =
+        start() |>
+        category(:success, ok: [])
+      
+      SmartGet.changeset(test_data, :ok)
+      |> assert_equal([:valid])
+    end
   end
-
-
-  test "any other category gets a `valid` check" do
-    test_data =
-      start() |>
-      category(:success,
-        ok: [changeset(no_changes: [:date])]
-      )
-
-    SmartGet.changeset(test_data, :ok)
-    |> assert_equal([:valid, {:no_changes, [:date]}])
-  end
-  
-  test "checks are added even if there's no changest" do
-    test_data =
-      start() |>
-      category(:success, ok: [])
-
-    SmartGet.changeset(test_data, :ok)
-    |> assert_equal([:valid])
-  end
-  
 end 
