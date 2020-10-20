bee216560835bd5a31933b3c82f03d2e57c43027
Brian Marick <marick@exampler.com>
2020-10-20 16:53:55 -0500

    Move as-cast checks into own file.

--- lib/impl/smart_get/changeset_checks.ex
+++ lib/impl/smart_get/changeset_checks.ex
@@ -2,7 +2,6 @@ defmodule TransformerTestSupport.Impl.SmartGet.ChangesetChecks do
   alias TransformerTestSupport.Impl.SmartGet
   alias SmartGet.Example
   alias SmartGet.ChangesetChecks, as: Checks
-  alias Ecto.Changeset
     
   @moduledoc """
   """
@@ -13,67 +12,11 @@ defmodule TransformerTestSupport.Impl.SmartGet.ChangesetChecks do
 
     changeset_checks
     |> Checks.Validity.add(example)
-    |> add_as_cast_checks(example, user_mentioned)
+    |> Checks.AsCast.add(example, user_mentioned)
   end
 
   def get(test_data, example_name) do
     Example.get(test_data, example_name)
     |> get
-  end
-  
-
-  defp insertion_changeset(example, fields) do
-    struct(example.metadata.module_under_test)
-    |> Changeset.cast(Example.params(example), fields)
-  end
-
-  defp add_as_cast_checks(changeset_checks, example, user_mentioned) do
-    case Keyword.get_values(example.metadata.field_transformations, :as_cast) do
-      [] ->
-        changeset_checks
-      lists ->
-        fields = Enum.concat(lists) |> Checks.Util.remove_fields_named_by_user(user_mentioned)
-        changeset = insertion_changeset(example, fields)
-        changeset_checks
-        |> combine(:changes, make_changes(changeset, fields))
-        |> combine(:no_changes, make_no_changes(changeset, fields))
-        |> combine(:errors, make_errors(changeset, fields))
-    end
-  end
-
-  def field({field, _value}), do: field
-  def field(field), do: field
-    
-
-  defp combine(so_far, _field, []), do: so_far
-
-  defp combine(so_far, field, values) do
-    so_far ++ [{field, values}]
-  end
-
-
-
-  def make_changes(changeset, fields) do
-    Enum.flat_map(fields, fn field ->
-      if field in Map.keys(changeset.changes),
-      do: [{field, changeset.changes[field]}],
-      else: []
-    end)
-  end
-
-  def make_no_changes(changeset, fields) do
-    Enum.flat_map(fields, fn field ->
-      if field in Map.keys(changeset.changes),
-      do: [],
-      else: [field]
-    end)
-  end
-
-  def make_errors(changeset, fields) do
-    Enum.flat_map(fields, fn field ->
-      if field in Keyword.keys(changeset.errors),
-      do: [{field, Keyword.get(changeset.errors, field) |> elem(0)}],
-      else: []
-    end)
   end
 end
--- lib/impl/smart_get/changeset_checks/as_cast.ex
+++ lib/impl/smart_get/changeset_checks/as_cast.ex
@@ -1,8 +1,68 @@
 defmodule TransformerTestSupport.Impl.SmartGet.ChangesetChecks.AsCast do
+  alias TransformerTestSupport.Impl.SmartGet
+  alias SmartGet.Example
+  alias SmartGet.ChangesetChecks, as: Checks
+  alias Ecto.Changeset
     
   @moduledoc """
   """
 
+  
+
+  defp insertion_changeset(example, fields) do
+    struct(example.metadata.module_under_test)
+    |> Changeset.cast(Example.params(example), fields)
+  end
+
+  def add(changeset_checks, example, user_mentioned) do
+    case Keyword.get_values(example.metadata.field_transformations, :as_cast) do
+      [] ->
+        changeset_checks
+      lists ->
+        fields = Enum.concat(lists) |> Checks.Util.remove_fields_named_by_user(user_mentioned)
+        changeset = insertion_changeset(example, fields)
+        changeset_checks
+        |> combine(:changes, make_changes(changeset, fields))
+        |> combine(:no_changes, make_no_changes(changeset, fields))
+        |> combine(:errors, make_errors(changeset, fields))
+    end
+  end
+
+  def field({field, _value}), do: field
+  def field(field), do: field
+    
+
+  defp combine(so_far, _field, []), do: so_far
+
+  defp combine(so_far, field, values) do
+    so_far ++ [{field, values}]
+  end
+
+
+
+  def make_changes(changeset, fields) do
+    Enum.flat_map(fields, fn field ->
+      if field in Map.keys(changeset.changes),
+      do: [{field, changeset.changes[field]}],
+      else: []
+    end)
+  end
+
+  def make_no_changes(changeset, fields) do
+    Enum.flat_map(fields, fn field ->
+      if field in Map.keys(changeset.changes),
+      do: [],
+      else: [field]
+    end)
+  end
+
+  def make_errors(changeset, fields) do
+    Enum.flat_map(fields, fn field ->
+      if field in Keyword.keys(changeset.errors),
+      do: [{field, Keyword.get(changeset.errors, field) |> elem(0)}],
+      else: []
+    end)
+  end
 
 
   
