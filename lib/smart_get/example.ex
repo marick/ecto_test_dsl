f08c5cf9faaa371720e6fa4263dd03436b4d6fc6
Brian Marick <marick@exampler.com>
2020-11-05 17:36:48 -0600

    More getters

--- lib/smart_get/example.ex
+++ lib/smart_get/example.ex
@@ -27,6 +27,8 @@ defmodule TransformerTestSupport.SmartGet.Example do
   def category_name(example), do: metadata(example, :category_name)
   def field_transformations(example), do: metadata(example, :field_transformations)
   def repo(example), do: metadata(example, :repo)
+  def setup(example), do: metadata(example, :setup)
+  def examples_module(example), do: metadata(example, :examples_module)
 
   def step_list(example) do
     example.metadata.category_workflows
