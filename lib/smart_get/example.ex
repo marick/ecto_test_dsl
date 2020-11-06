5147b2a5234162c4320c67c839b6ff01596cc686
Brian Marick <marick@exampler.com>
2020-11-06 16:57:41 -0600

    Distinguish between required and optional metadata

--- lib/smart_get/example.ex
+++ lib/smart_get/example.ex
@@ -17,18 +17,26 @@ defmodule TransformerTestSupport.SmartGet.Example do
     end
   end
 
-  def metadata(example, field),
+  def metadata!(example, field),
     do: Map.fetch!(example.metadata, field)
 
-  def step_functions(example), do: metadata(example, :steps)
-  def module_under_test(example), do: metadata(example, :module_under_test)
-  def format(example), do: metadata(example, :format)
-  def name(example), do: metadata(example, :name)
-  def category_name(example), do: metadata(example, :category_name)
-  def field_transformations(example), do: metadata(example, :field_transformations)
+  def metadata(example, field),
+    do: Map.get(example.metadata, field)
+
+  
+
+  def step_functions(example), do: metadata!(example, :steps)
+  def module_under_test(example), do: metadata!(example, :module_under_test)
+  def format(example), do: metadata!(example, :format)
+  def name(example), do: metadata!(example, :name)
+  def category_name(example), do: metadata!(example, :category_name)
+  def field_transformations(example), do: metadata!(example, :field_transformations)
+  def setup(example), do: metadata!(example, :setup)
+  def examples_module(example), do: metadata!(example, :examples_module)
+
+
   def repo(example), do: metadata(example, :repo)
-  def setup(example), do: metadata(example, :setup)
-  def examples_module(example), do: metadata(example, :examples_module)
+  
 
   def step_list(example) do
     example.metadata.category_workflows
