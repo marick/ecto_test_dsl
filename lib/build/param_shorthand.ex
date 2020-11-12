2fc882c512740847c4a2a71d0643b6e1a87609ff
Brian Marick <marick@exampler.com>
2020-11-11 18:09:19 -0600

    `id_of` adds setup code.

--- lib/build/param_shorthand.ex
+++ lib/build/param_shorthand.ex
@@ -12,16 +12,51 @@ defmodule TransformerTestSupport.Build.ParamShorthand do
 
   def expand(example, :example, existing_pairs) do
     example
-    |> expand_interior(:params, existing_pairs)
+    |> expand_like(existing_pairs)
+    |> add_setup
   end
 
-  def expand_interior(example, field, existing_pairs) do
-    field_value = Map.get(example, field)
-    case is_function(field_value) do
+  def expand_like(example, existing_pairs) do
+    params = Map.get(example, :params, [])
+    case is_function(params) do
       true ->
-        Map.put(example, field, field_value.(existing_pairs))
+        Map.put(example, :params, params.(existing_pairs))
       false ->
         example
     end
   end
+
+  def add_setup(example) do
+    params = Map.get(example, :params, [])
+    old_setups = Map.get(example, :setup, [])
+
+    new_setups =
+      params
+      |> Enum.filter(&example_reference?/1)
+      |> Enum.map(fn {_, {_, extended_example_name, _}} ->
+          {:insert, extended_example_name}
+         end)
+
+    case {old_setups, new_setups} do
+      {_, []} ->
+        example
+      {[], _} -> 
+        Map.put(example, :setup, new_setups)
+      {_, _} ->
+        Map.put(example, :setup, old_setups ++ new_setups)
+    end
+  end
+
+  @setup_reference :__setup_reference
+
+  def example_reference?({_key, value}) do
+    is_tuple(value) && elem(value, 0) == @setup_reference
+  end
+  def example_reference?(_), do: false
+
+  def setup_reference(extended_example_name, use_type),
+    do: {@setup_reference, extended_example_name, use_type}
+    
+  
+    
 end
--- test/build/param_shorthand_test.exs
+++ test/build/param_shorthand_test.exs
@@ -1,8 +1,10 @@
 defmodule Build.ParamsShorthandTest do
-  use TransformerTestSupport.Case
-  use TransformerTestSupport.Predefines
+  alias TransformerTestSupport, as: T
+  use T.Case
+  use T.Predefines
+  alias T.Build
 
-  test "like" do
+  test "params_like" do
     start()
     |> category(:valid, ok: [                    params(a: 1, b: 2)])
     |> category(:invalid, similar: [params_like(:ok, except: [b: 4])])
@@ -10,7 +12,7 @@ defmodule Build.ParamsShorthandTest do
     |> assert_field(params: %{a: 1, b: 4})
   end
 
-  test "using like to refer to values within the same category" do
+  test "using params_like to refer to values within the same category" do
     start()
     |> category(:valid, [
           ok: [params: %{a: 1, b: 2}],
@@ -36,12 +38,38 @@ defmodule Build.ParamsShorthandTest do
     assert example(actual, :different).params == %{a: 1, b: 2, c: 383}
   end
 
-  test "like can copy everything" do
+  test "params_like can copy everything" do
     actual = 
       start()
       |> category(:valid, ok: [params(a: 1, b: 2)])
       |> category(:invalid, similar: [params_like(:ok)])
 
     assert example(actual, :ok).params == example(actual, :similar).params
+  end
+
+  test "instances of `id_of` generate a setup" do
+    actual = 
+      start()
+      |> category(:valid, ok: [params(a: 1, b: 2)])
+      |> category(:invalid, similar: [
+           params_like(:ok, except: [a: id_of(species: ExampleModule)])
+         ])
+
+    assert example(actual, :similar).setup == [insert: {:species, ExampleModule}]
+  end
+
+  test "adds on to existing setup" do
+    actual = 
+      start()
+      |> category(:invalid, name: [
+           params(a: id_of(species: ExampleModule),
+                  b: id_of(:thing)),
+           Build.setup(insert: :noog)
+        ])
+
+    assert example(actual, :name).setup ==
+        [insert: :noog,
+         insert: {:species, ExampleModule},
+         insert: {:thing, __MODULE__}]
   end
 end  
