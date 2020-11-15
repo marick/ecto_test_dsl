9c611b36fc55f360c45f61eaaa4fff87825fd3a6
Brian Marick <marick@exampler.com>
2020-11-15 14:16:50 -0600

    Further progress on separating constraint and validation changesets

--- lib/variant_support/changeset_support.ex
+++ lib/variant_support/changeset_support.ex
@@ -17,7 +17,7 @@ defmodule TransformerTestSupport.VariantSupport.ChangesetSupport do
 
   def check_validation_changeset(running, changeset_step) do 
     changeset = RunningExample.step_value!(running, changeset_step)
-    check_changeset(changeset, running, :changeset_for_validation_step)
+    check_validation_changeset_(changeset, running)
   end
   
   # ----------------------------------------------------------------------------
@@ -65,7 +65,7 @@ defmodule TransformerTestSupport.VariantSupport.ChangesetSupport do
   def check_constraint_changeset(running, changeset_step) do
     case RunningExample.step_value!(running, changeset_step) do
       {:error, changeset} -> 
-        check_changeset(changeset, running, :changeset_for_constraint_step)
+        check_constraint_changeset_(changeset, running)
       tuple -> 
         elaborate_flunk(
           context(running.example, "Expected an error tuple containing a changeset"),
@@ -76,11 +76,23 @@ defmodule TransformerTestSupport.VariantSupport.ChangesetSupport do
 
   # ----------------------------------------------------------------------------
 
-  defchain check_changeset(changeset, running, step) do
+  defchain check_validation_changeset_(changeset, running) do
     prior_work = Keyword.get(running.history, :repo_setup, %{})
     adjust_assertion_message(
       fn ->
-        for check <- ChangesetChecks.get(running.example, step, previously: prior_work),
+        for check <- ChangesetChecks.get_validation_checks(running.example, previously: prior_work),
+          do: apply_assertion(changeset, check)
+      end,
+      fn message ->
+        error_message(running.example, changeset, message)
+      end)
+  end
+
+  defchain check_constraint_changeset_(changeset, running) do
+    prior_work = Keyword.get(running.history, :repo_setup, %{})
+    adjust_assertion_message(
+      fn ->
+        for check <- ChangesetChecks.get(running.example, :changeset_for_constraint_step, previously: prior_work),
           do: apply_assertion(changeset, check)
       end,
       fn message ->
