4bffd88475a06bd72208491cc7946cbddcd280f1
Brian Marick <marick@exampler.com>
2020-11-18 17:43:29 -0600

    magritte dependency

--- mix.exs
+++ mix.exs
@@ -68,6 +68,7 @@ defmodule TransformerTestSupport.MixProject do
       {:ecto_sql, "~> 3.4"},      
       {:deep_merge, "~> 1.0"},      
       {:ex_machina, "~> 2.3", only: :test},
+      {:magritte, "~> 0.1.2"},
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
