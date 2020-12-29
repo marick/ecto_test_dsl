6d4c36c54a3d99d1603f0e78e114785c7896486f
Brian Marick <marick@exampler.com>
2020-12-29 14:21:51 -0600

    Removed duplicate code.

--- lib/10_parse/build/key_validation.ex
+++ lib/10_parse/build/key_validation.ex
@@ -1,19 +1,13 @@
 defmodule TransformerTestSupport.Build.KeyValidation do
-  alias TransformerTestSupport.Messages
+  use TransformerTestSupport.Drink.Me
+  alias T.Messages
   import FlowAssertions.Define.{Defchain,BodyParts}
   
   defchain assert_valid_keys(map, required, optional) do
-    missing = diff(required, Map.keys(map))
-    extras = diff(Map.keys(map), optional) |> diff(required)
+    missing = EnumX.difference(required, Map.keys(map))
+    extras = EnumX.difference(Map.keys(map), optional) |> EnumX.difference(required)
     elaborate_assert(Enum.all?([missing, extras], &Enum.empty?/1),
       Messages.invalid_keys,
       left: [missing: missing, extras: extras])
-  end
-
-
-  defp diff(larger, smaller) do
-    larger_set = MapSet.new(larger)
-    smaller_set = MapSet.new(smaller)
-    MapSet.difference(larger_set, smaller_set) |> Enum.into([])
   end
 end
