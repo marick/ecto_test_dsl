9798eab4728a5a98230a01db983c693bfff65ca7
Brian Marick <marick@exampler.com>
2020-12-29 21:36:42 -0600

    Extract on_success

--- lib/10_parse/build.ex
+++ lib/10_parse/build.ex
@@ -19,39 +19,8 @@ defmodule TransformerTestSupport.Build do
   # ----------------------------------------------------------------------------
 
 
-  # ----------------------------------------------------------------------------
 
 
-  defmacro on_success(funcall) do
-    from = "on_success(#{Macro.to_string(funcall)})"
-    case Macro.decompose_call(funcall) do
-      {{:__aliases__, _, aliases},  fun_atom, args} -> 
-        composed_module = Enum.reduce(aliases, :Elixir, fn alias, acc ->
-          Module.safe_concat(acc, alias)
-        end)
-        fun = Function.capture(composed_module, fun_atom, length(args))
-        quote do
-          FieldCalculator.new(unquote(fun), unquote(args), unquote(from))
-        end
-
-      {fun_atom, args} ->
-        quote do 
-          fun = Function.capture(__MODULE__, unquote(fun_atom), length(unquote(args)))
-          FieldCalculator.new(fun, unquote(args), unquote(from))
-        end
-
-      _ ->
-        raise """
-        The argument to `on_success/1` does not look like a function call.
-        You may want the `on_success(f, applied_to: args)` variant.
-        """
-    end
-  end
-
-  def on_success(f, applied_to: fields) when is_list(fields),
-    do: FieldCalculator.new(f, fields, "on_success(<fn>, applied_to: #{inspect fields})")
-  def on_success(f, applied_to: field),
-    do: on_success(f, applied_to: [field])
 
 
 
---
+++ test/10_parse/internal_functions/field_calculation_test.exs
@@ -0,0 +1,32 @@
+defmodule Parse.InternalFunctions.FieldCalculationTest do
+  use TransformerTestSupport.Case
+  use T.Predefines
+
+  def function_in_module(x), do: x - 3
+
+  test "on_success" do
+    
+    on_success(Date.diff(:date, ~D[2000-01-01]))
+    |> assert_fields(calculation: &Date.diff/2,
+                     args: [:date, ~D[2000-01-01]],
+                     from: "on_success(Date.diff(:date, ~D[2000-01-01]))")
+                          
+    on_success(List.Chars.to_charlist(:date))
+    |> assert_fields(calculation: &List.Chars.to_charlist/1,
+                     args: [:date],
+                     from: "on_success(List.Chars.to_charlist(:date))")
+                          
+
+    on_success(function_in_module(:date))
+    |> assert_fields(calculation: &__MODULE__.function_in_module/1,
+                     args: [:date],
+                     from: "on_success(function_in_module(:date))")
+    
+    # The variant that accepts functions
+    f = &(&1 + 1)
+    on_success(f, applied_to: :date)
+    |> assert_fields(calculation: f,
+                     args: [:date],
+                     from: "on_success(<fn>, applied_to: [:date])")
+  end
+end
