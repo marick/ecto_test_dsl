a78df45092a358fd2e8fb5526d80dbf1a691472b
Brian Marick <marick@exampler.com>
2021-01-05 16:05:56 -0600

    Cleanup `previously`

---
+++ lib/10_parse/50_previously.ex
@@ -0,0 +1,39 @@
+defmodule TransformerTestSupport.Parse.Previously do
+  use TransformerTestSupport.Drink.Me
+
+  # ----------------------------------------------------------------------------
+  # Working with a container of one or more example sources
+
+
+  def ensure_references(named_examples, default_module) do
+    KeywordX.map_over_values(named_examples, fn example ->
+      ensure_one_example(example, default_module)
+    end)
+  end
+
+  def ensure_one_example(example, default_module) do
+    Map.update(example, :setup_instructions, [], fn instructions ->
+      Enum.flat_map(instructions, &(expand_instructions &1, default_module))
+    end)
+  end
+
+  def expand_instructions({:insert, {name, module}}, _) do
+    [insert: een(name, module)]
+  end
+
+  def expand_instructions({:insert, name}, default_module) when is_atom(name) do
+    [insert: een(name, default_module)]
+  end
+
+  def expand_instructions({:insert, names}, default_module) when is_list(names) do
+    Enum.flat_map(names, fn name ->
+      expand_instructions({:insert, name}, default_module)
+    end)
+  end
+
+  def expand_instructions(instructions, default_module) when is_list(instructions) do
+    Enum.flat_map(instructions, fn instruction -> 
+      expand_instructions(instruction, default_module)
+    end)
+  end
+end
--- test/10_parse/20_top_level/previously_test.exs
+++ test/10_parse/20_top_level/previously_test.exs
@@ -1,9 +1,6 @@
 defmodule Parse.PreviouslyTest do
   use TransformerTestSupport.Case
   use T.Parse.All
-  # alias T.Variants.EctoClassic
-  # alias T.Run.Steps
-
 
   def expect(input, expected) do
     %{module_under_test: Schema, examples_module: Examples, examples: []}
@@ -14,7 +11,7 @@ defmodule Parse.PreviouslyTest do
     |> assert_equal(expected)
   end
 
-  test "foo" do
+  test "expansion of `previously` arguments" do
     previously(insert: :a)         |> expect([insert: een(a: Examples)])
     previously(insert: [:a, :b]) |> expect([insert: een(a: Examples),
                                             insert: een(b: Examples)])
@@ -30,107 +27,4 @@ defmodule Parse.PreviouslyTest do
                                                       insert: een(:b, Examples),
                                                       insert: een(:c, Other)])
   end
-
-
-
-  # defmodule Schema do
-  #   use Ecto.Schema
-  #   import Ecto.Changeset
-
-  #   embedded_schema do
-  #     field :name, :string
-  #   end
-
-  #   def changeset(struct, params) do
-  #     struct
-  #     |> cast(params, [:name])
-  #     |> validate_required([:name])
-  #   end
-
-  #   def named(name),
-  #     do: %Schema{name: Test.Factory.unique(name), id: "#{name}_id"}
-  # end
-
-  # defmodule Examples do
-  #   use EctoClassic.Insert
-
-  #   def fake_insert(changeset),
-  #     do: {:ok, Schema.named(changeset.changes.name)}
-
-  #   def make(name),
-  #     do: {name, [params(name: to_string(name))]}
-  #   def make(name, one_previously),
-  #     do: {name, [params(name: to_string(name)), one_previously]}
-
-  #   def create_test_data do 
-  #     start(
-  #       module_under_test: Schema,
-  #       repo: Unused
-  #     ) |>
-
-  #     replace_steps(insert_changeset: step(&fake_insert/1, :make_changeset)) |> 
-      
-  #     workflow(                                         :success, [
-  #       make(:leaf),
-  #       make(:leaf2),
-  #       make(:dependent, previously(insert:  [leaf: __MODULE__])),
-  #       make(:depth_3,   previously(insert:   :dependent)),
-
-  #       make(:breadth_2, previously(insert:          [:leaf,   # one insert, two examples
-  #                                                :leaf2])), 
-  #       make(:insert_then_insert, previously(insert: :depth_3, # two inserts
-  #                                       insert: :leaf2)),
-
-  #       make(:has_duplicates, previously(insert: :depth_3, insert: :dependent))
-  #     ])
-  #     end
-  # end
-
-  # defmodule ActualTests do
-  #   use TransformerTestSupport.Case
-
-  #   # Note: this won't necessarily prevent races if any other tests
-  #   # use these names.
-  #   def start_names_with_zero() do
-  #     Examples.Tester.test_data.examples
-  #     |> Keyword.keys
-  #     |> Enum.map(&to_string/1)
-  #     |> ExMachina.Sequence.reset
-  #   end
-      
-
-  #   def setup_for(example_name) do
-  #     start_names_with_zero()
-  #     Examples.Tester.check_workflow(example_name)
-  #     |> Keyword.get(:previously)
-  #   end
-
-  #   # Note: `setup_for` and `expect` are a bit tricksy. Each example is
-  #   # supposed to only be created once. By resettting ExMachina.Sequence
-  #   # before running the test and checking the actual result,
-  #   # a duplicate creation will produce a different final name in the two.
-
-  #   def expect(actual, names) do
-  #     start_names_with_zero()
-      
-  #     expected = 
-  #       names
-  #       |> Enum.map(fn name ->
-  #                     {een(name, Examples), Schema.named(to_string name)}
-  #                   end)
-  #       |> Map.new
-  #     assert actual == expected
-  #   end
-    
-  #   # ----------------------------------------------------------------------------
-
-  #   test "insertions" do
-  #     setup_for(:leaf)      |> expect([])  # no setup clause
-  #     setup_for(:dependent) |> expect([:leaf]) # depends on one other value
-  #     setup_for(:breadth_2) |> expect([:leaf, :leaf2])  # depends on two
-  #     setup_for(:depth_3)   |> expect([:leaf, :dependent]) # recurses
-  #     setup_for(:insert_then_insert) |> expect([:depth_3, :dependent, :leaf, :leaf2])
-  #     setup_for(:has_duplicates)     |> expect([:depth_3, :dependent, :leaf])
-  #   end
-  # end
 end
