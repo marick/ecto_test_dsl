c732b838fbdadb0c25741c951dc40c88dad16c48
Brian Marick <marick@exampler.com>
2021-01-06 12:27:14 -0600

    Moving toward get_validation_checks rewrite.

--- lib/smart_get/changeset_checks.ex
+++ lib/smart_get/changeset_checks.ex
@@ -11,26 +11,29 @@ defmodule TransformerTestSupport.SmartGet.ChangesetChecks do
   """
 
   def get_validation_checks(example, previously: previously) do
-    changeset_checks = Map.get(example, :changeset_for_validation_step, [])
-    user_mentioned = CC.unique_fields(changeset_checks)
+    example_specific_checks = Map.get(example, :changeset_for_validation_step, [])
+    user_mentioned = CC.unique_fields(example_specific_checks)
 
     resolved_params = Params.get(example, previously: previously)
+
+    with_valid = 
+      add_whole_changeset_check([], example)
 
     as_cast_checks =
       Example.metadata!(example, :as_cast)
       |> AsCast.subtract(user_mentioned)
       |> AsCast.changeset_checks(resolved_params)
 
-    [_as_cast_fields, calculated_fields] =
-      Checks.Util.separate_types_of_transformed_fields(example)
-      |> Enum.map(&(Checks.Util.remove_fields_named_by_user(&1, user_mentioned)))
+    calculated_fields = 
+      Example.metadata!(example, :field_calculators)
+      |> Enum.reject(&Enum.member?(user_mentioned, &1))
 
-    with_valid = 
-      changeset_checks
-      |> add_whole_changeset_check(example)
+    calculated_checks = 
+      FieldCalculation.add([], example, calculated_fields)
 
-    (with_valid ++ as_cast_checks)
-    |> FieldCalculation.add(example, calculated_fields)
+
+    (with_valid ++       example_specific_checks
+      ++ as_cast_checks ++ calculated_checks)
   end
 
   defp add_whole_changeset_check(checks_so_far, example) do
@@ -46,8 +49,8 @@ defmodule TransformerTestSupport.SmartGet.ChangesetChecks do
   # refer to previous examples.
   def get_constraint_checks(example, opts \\ []) do
     _previously = Keyword.get(opts, :previously, %{})
-    changeset_checks = Map.get(example, :changeset_for_constraint_step, [])
+    example_specific_checks = Map.get(example, :changeset_for_constraint_step, [])
 
-    changeset_checks
+    example_specific_checks
   end
 end
