def45264c7323b87a86b7b09d86405f20ee56718
Brian Marick <marick@exampler.com>
2021-01-11 17:01:16 -0600

    Better version of `given`

--- test/support/given.ex
+++ test/support/given.ex
@@ -3,16 +3,22 @@ defmodule Given do
   alias ExUnit.Assertions
 
   defmodule Util do
+    # Note that this doesn't resolve aliases, which requires the __ENV__ at
+    # macroexpansion time. Haven't figured out how to make use of that
+    # information within a single function, so the caller will have to
+    # adjust the result if the first part of the function is a module
+    # alias.
+    
     def decompose_call(funcall, default_module) do
       case Macro.decompose_call(funcall) do
         {{:__aliases__, _, aliases},  fun_atom, args} -> 
-          composed_module =
+          composed_alias =
             Enum.reduce(aliases, :Elixir, fn alias, acc ->
               Module.safe_concat(acc, alias)
             end)
           function_description = [{fun_atom, length(args)}]
 
-          {composed_module, function_description, args}
+          {composed_alias, function_description, args}
           
         {fun_atom, args} ->
           function_description = [{fun_atom, length(args)}]
@@ -26,10 +32,8 @@ defmodule Given do
       end
     end
 
-    def key_ast({module, function_description, arglist}) do
-      quote do 
-        {Given, unquote(module), unquote(function_description), unquote(arglist)}
-      end
+    def key({module, function_description, arglist}) do
+      {Given, module, function_description, arglist}
     end
 
     def process_fetch!(key) do
@@ -47,36 +51,74 @@ defmodule Given do
           value
       end
     end
-    
 
-    @args [:a1, :a2, :a3, :a4, :a5, :a6, :a7, :a8, :a9, :aa, :ab, :ac, :ad, :ae, :af]
+    # Note: This could be replaced by a `return_function_ast` that
+    # uses `unquote_splicing` to construct a single function. However,
+    # the function requires access to the compile-time __ENV__, so it
+    # must be within a quote, and I don't know how to splice the
+    # resulting AST into the `quote do`. Probably there's a way, or it
+    # could be done by using data-manipulation functions (Map, List)
+    # on the AST treated as a data stucture. But I've spent enough
+    # time on this.
+    #
+    # Actual AST-creating code is commented out below.
 
-    def return_function_ast({module, function_description, arglist}) do
-      vars = for a <- Enum.take(@args, length(arglist)), do: Macro.var(a, __MODULE__)
+    def return_function({module, function_description, arglist}) do
+      fetcher = fn vars ->
+        Given.Util.process_fetch!({Given, module, function_description, vars})
+      end
 
-      quote do 
-        fn unquote_splicing(vars) ->
-          Given.Util.process_fetch!({
-            Given,
-            unquote(module),
-            unquote(function_description),
-            unquote(vars)
-          })
-        end
+      case length(arglist) do
+        0 -> fn                        -> fetcher.([                      ]) end
+        1 -> fn a1                     -> fetcher.([a1                    ]) end
+        2 -> fn a1, a2                 -> fetcher.([a1, a2                ]) end
+        3 -> fn a1, a2, a3             -> fetcher.([a1, a2, a3            ]) end
+        4 -> fn a1, a2, a3, a4         -> fetcher.([a1, a2, a3, a4        ]) end
+        5 -> fn a1, a2, a3, a4, a5     -> fetcher.([a1, a2, a3, a4, a5    ]) end
+        6 -> fn a1, a2, a3, a4, a5, a6 -> fetcher.([a1, a2, a3, a4, a5, a6]) end
+        _ ->
+          Assertions.flunk(
+            """
+            For essentially arbitrary reasons, `given` works only for
+            functions with six or fewer args. This is easily changed.
+            """)
       end
     end
+
+    # @args [:a1, :a2, :a3, :a4, :a5, :a6, :a7, :a8, :a9, :aa, :ab, :ac, :ad, :ae, :af]
+
+    # def return_function_ast({module, function_description, arglist}) do
+    #   vars = for a <- Enum.take(@args, length(arglist)), do: Macro.var(a, __MODULE__)
+
+    #   quote do 
+    #     fn unquote_splicing(vars) ->
+    #       Given.Util.process_fetch!({
+    #         Given,
+    #         unquote(module),
+    #         unquote(function_description),
+    #         unquote(vars)
+    #       })
+    #     end
+    #   end
+    # end
+    
   end
+
+
+  
   
   defmacro given(funcall, return: body) do
-    {module, function_description, _arglist} = triple = 
+    {aliass, function_description, arglist} = 
       Given.Util.decompose_call(funcall, __MODULE__)
 
-    key = Util.key_ast(triple)
-    value_calculator = Util.return_function_ast(triple)
-
     quote do
-      Process.put(unquote(key), unquote(body))
-      mock(unquote(module), unquote(function_description), unquote(value_calculator))
+      module = Keyword.get(__ENV__.aliases, unquote(aliass), unquote(aliass))
+      triple = {module, unquote(function_description), unquote(arglist)}
+
+      key = Given.Util.key(triple)
+      value_calculator = Util.return_function(triple)
+      Process.put(key, unquote(body))
+      mock(module, unquote(function_description), value_calculator)
     end
   end
 
--- test/support/given_test.exs
+++ test/support/given_test.exs
@@ -3,27 +3,43 @@ defmodule GivenTest do
   import Mockery.Macro
   use Given
 
-  describe "decomposing a function call" do 
-    test "with module" do
-      input = quote do: Process.add(:key, "value")
+  describe "decomposing a function call" do
+    defp expect(input, expected) do
       actual = Given.Util.decompose_call(input, __MODULE__)
-      expected = {Process, [add: 2], [:key, "value"]}
       assert actual == expected
+    end
+    
+    test "straightforward cases" do
+      # without module
+      (quote do:               to_string(      5))
+      |> expect({__MODULE__, [to_string: 1], [5]})
+
+      # Single module
+      (quote do:  Process.add(        :key, "value"))
+      |> expect({Process, [add: 2], [:key, "value"]})
+
+      # Nested module
+      (quote do: String.Chars.to_string(4))
+      |> expect({String.Chars, [to_string: 1], [4]})
     end
 
-    test "without module" do
-      input = quote do: to_string(5)
-      actual = Given.Util.decompose_call(input, __MODULE__)
-      expected = {__MODULE__, [to_string: 1], [5]}
-      assert actual == expected
+    test "aliased module requires later work" do
+      alias String.Chars
+
+      # Note that it expands to the top-level Elixir.Chars because
+      # `decompose_call` doesn't have access to the alias information
+      # in `__ENV__`.
+      (quote do: Chars.to_string(        4))
+      |> expect({Elixir.Chars, [to_string: 1], [4]})
     end
+    
   end
 
-  def function_under_test(count) do
+  def function_under_test_uses_date(count) do
     {:ok, mockable(Date).add(~D[2001-02-03], count)}
   end
 
-  test "given" do
+  test "given with a simple non-aliased module" do
     # Manifest constants
     given Date.add(~D[2001-02-03], 3), return: "return for 3"
 
@@ -31,20 +47,49 @@ defmodule GivenTest do
     date = ~D[2001-02-03]
     given(Date.add(date, 1+3), return: "return for 4")
 
-    assert function_under_test(3) == {:ok, "return for 3"}
-    assert function_under_test(4) == {:ok, "return for 4"}
+    assert function_under_test_uses_date(3) == {:ok, "return for 3"}
+    assert function_under_test_uses_date(4) == {:ok, "return for 4"}
+  end
+
+  def function_under_test_uses_string_dot_chars(arg) do
+    {:ok, mockable(String.Chars).to_string(arg)}
+  end
+
+  test "given with a compound non-aliased module" do
+    given String.Chars.to_string(3), return: "return for 3 S.C"
+
+    assert function_under_test_uses_string_dot_chars(3) == {:ok, "return for 3 S.C"}
+  end
+
+  alias String.Chars
+  def function_under_test_uses_chars(arg) do
+    {:ok, mockable(Chars).to_string(arg)}
+  end
+
+  test "given with an aliased module" do
+    given Chars.to_string(3), return: "return for 3 C"
+
+    assert function_under_test_uses_string_dot_chars(3) == {:ok, "return for 3 C"}
+  end
+
+  test "aliased and unaliased versions resolve to the same module" do
+    given String.Chars.to_string(3), return: "return for 3 S.C"
+    assert function_under_test_uses_chars(3) == {:ok, "return for 3 S.C"}
+
+    given Chars.to_string(3), return: "return for 3 C"
+    assert function_under_test_uses_string_dot_chars(3) == {:ok, "return for 3 C"}
   end
 
   test "a missing value flunks the test" do
     # Note that the flunking only happens if the function has been stubbed.
-    assert {:ok, _date} = function_under_test(5)
+    assert {:ok, _date} = function_under_test_uses_date(5)
 
     given Date.add(~D[2001-02-03], 3), return: "return for 3"
 
     error =
       assert_raise(ExUnit.AssertionError,
         fn ->
-          function_under_test(5)
+          function_under_test_uses_date(5)
         end)
     assert String.contains?(error.message,
       "You did not set up a stub for Date.add(~D[2001-02-03], 5)")
