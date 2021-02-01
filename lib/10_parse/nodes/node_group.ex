bcbdfc653c5dd8f6544f6cde534351ebee5bcdb1
Brian Marick <marick@exampler.com>
2021-02-01 16:31:57 -0600

    Generalize NodeGroup

--- lib/10_parse/nodes/node_group.ex
+++ lib/10_parse/nodes/node_group.ex
@@ -3,24 +3,26 @@ defmodule EctoTestDSL.Parse.Node.Group do
   alias T.Parse.Node
 
   def handle_eens(example, default_module) do
-    new_example = update_eenable(example, default_module)
+    new_example = 
+      update_for_protocol(example, Node.EENAble,
+        &(Node.EENable.ensure_eens(&1, default_module)))
+    
     eens = accumulated_eens(new_example)
-
     Map.put(new_example, :eens, eens)
   end
 
-  defp eenable_keys(example) do
+  defp keys_for_protocol(example, protocol) do 
     example
-    |> KeywordX.filter_by_value(&Node.EENable.impl_for/1)
+    |> KeywordX.filter_by_value(&protocol.impl_for/1)
     |> Enum.map(fn {key, _value} -> key end)
   end
 
-  defp update_eenable(example, default_module) do
+  defp update_for_protocol(example, protocol, f) do
     reducer = fn key, acc ->
-      Map.update!(acc, key, &(Node.EENable.ensure_eens(&1, default_module)))
+      Map.update!(acc, key, f)
     end
 
-    eenable_keys(example)
+    keys_for_protocol(example, Node.EENable)
     |> Enum.reduce(example, reducer)
   end
 
@@ -29,7 +31,7 @@ defmodule EctoTestDSL.Parse.Node.Group do
       Map.get(example, key) |> Node.EENable.eens
     end
 
-    eenable_keys(example)
+    keys_for_protocol(example, Node.EENable)
     |> Enum.flat_map(getter)
   end
 end
