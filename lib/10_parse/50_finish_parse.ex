72d0544ceff68ed7cb6a59931e91de6613c868a7
Brian Marick <marick@exampler.com>
2021-02-04 11:49:08 -0600

    Clean up `finish`

--- lib/10_parse/50_finish_parse.ex
+++ lib/10_parse/50_finish_parse.ex
@@ -9,29 +9,28 @@ defmodule EctoTestDSL.Parse.FinishParse do
   """
 
   def finish(test_data) do
-    examples = test_data.examples
-    examples_module = test_data.examples_module
-
-    new_test_data = 
-      Enum.reduce(examples, test_data, fn {name, example}, acc ->
-        improved = 
-          example
-          |> propagate_metadata(test_data)
-          |> Node.Group.parse_time_substitutions(acc.examples)
-          |> Map.update(:previously, [], &Node.Previously.parse/1)
-          |> Node.Group.handle_eens(examples_module)
-        
-        # Note: it is important for each example to be put in the
-        # test_data map as it's finished because later examples can
-        # refer back to earlier ones.
-        put_in(acc, [:examples, name], improved)
-      end)
-
-    simplified = KeywordX.map_over_values(new_test_data.examples, &Node.Group.simplify/1)
-    Map.put(test_data, :examples, simplified)
+    test_data.examples
+    |> transform_examples(test_data)
+    |> KeywordX.map_over_values(&Node.Group.simplify/1)
+    |> Map.put(test_data, :examples, ...)
   end
 
-  def propagate_metadata(example, test_data) do
+  defp transform_examples(examples, metadata) do
+    examples    
+    |> Enum.reduce(examples, fn {name, example}, acc ->
+      example
+      |> propagate_metadata(metadata)
+      |> Node.Group.parse_time_substitutions(acc)
+      |> Map.update(:previously, [], &Node.Previously.parse/1)
+      |> Node.Group.handle_eens(metadata.examples_module)
+      # Note: it is important for each example to be put in the
+      # keyword list as it's finished because later examples can
+      # refer back to earlier ones.
+      |> put_in(acc, [name], ...)
+    end)
+  end
+
+  defp propagate_metadata(example, test_data) do
     metadata = Map.delete(test_data, :examples) # Let's not have a recursive structure.
     deep_merge(example, %{metadata: metadata})
   end
