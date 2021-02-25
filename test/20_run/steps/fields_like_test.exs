264b66c8a889469e16c81a461adc4ef240008587
Brian Marick <marick@exampler.com>
2021-02-25 15:59:27 -0600

    Finish `field_checks`

--- test/20_run/steps/fields_like_test.exs
+++ test/20_run/steps/fields_like_test.exs
@@ -13,135 +13,157 @@ defmodule Run.Steps.FieldsLikeTest do
     :ok
   end
 
-  defp run(~M{actual, opts}) do 
-    stub_history(updated_value: actual)
-    stub(fields_like: Rnode.FieldsLike.new(een(:inserted), opts))
-    Steps.field_checks(:running, :updated_value)
+  defp run([{:after, neighborhood},
+            {:compare, new_struct},
+            {:against, reference_struct} | adjustments]) do
+    stub(neighborhood:
+      Map.put(neighborhood, een(:reference_struct), reference_struct))
+    
+    instructions = Rnode.FieldsLike.new(een(:reference_struct), adjustments)
+    stub(fields_like: instructions)
+
+    stub_history(new_struct: new_struct)
+    Steps.field_checks(:running, :new_struct)
   end
+
+  defp run([{:compare, new_struct}, {:against, reference_struct} | adjustments]) do
+    run([{:after, %{}}, {:compare, new_struct},
+         {:against, reference_struct} | adjustments])
+  end
+  
 
   defp pass(setup), do: assert run(setup) == :uninteresting_result
 
-  describe "fields_like" do
-    test "just an een: pass" do
-      stub(neighborhood: %{een(:inserted) => %{a: 5}})
-      %{actual:                              %{a: 5}, opts: []} |> pass()
-    end
+  describe "fields_like basics" do
+    test "just an een" do
+      [compare: %{a: 5},
+       against: %{a: 5}] |> pass()
 
-    test "just an een: fail" do
-      stub(neighborhood: %{een(:inserted) => %{a: 4}})
-      input = %{actual:                      %{a: 5}, opts: []}
-
+      bad_field = [compare: %{a: 5},
+                   against: %{a: 4}]
+      
       assertion_fails(~r/Example `:example`/,
         [message: ~r/Assertion with == failed/,
          left:  %{a: 5},
          right: %{a: 4}],
         fn ->
-          run(input)
+          run(bad_field)
         end)
-    end
 
-    test "just an een: extra fields" do
-      stub(neighborhood: %{een(:inserted) => %{a: 5}})
-      input = %{actual:                      %{a: 5, b: 4}, opts: []}
-
+      extra_field = [compare: %{a: 5, extra: 1},
+                     against: %{a: 5          }]
+      
       assertion_fails(~r/Example `:example`/,
         [message: ~r/Assertion with == failed/,
-         left:  %{a: 5, b: 4},
+         left:  %{a: 5, extra: 1},
          right: %{a: 5}],
         fn ->
-          run(input)
+          run(extra_field)
         end)
     end
     
-    test "`ignoring` works" do
-      stub(neighborhood: %{een(:inserted) => %{a: 5}})
-      input = %{
-        actual:                              %{a: 5, lock_value: 58},
-        opts:                           [ignoring: [:lock_value]]
-      }
-
-      input |> pass()
+    test "`ignoring` option" do
+      [compare: %{a: 5,     lock_value: 58},
+       against: %{a: 5                    },
+                 ignoring: [:lock_value]] |> pass()
     end
 
-    test "`except` works" do
-      stub(neighborhood: %{een(:inserted) => %{a: 5, special: "old"}})
-
-      input = %{
-        actual:                              %{a: 5, special: "new"},
-        opts:                              [except: [special: "new"]]
-      }
-
-      input |> pass()
+    test "`except` option" do
+      [compare: %{a: 5,  special: "new"},
+       against: %{a: 5,  special: "old"},
+                except: [special: "new"]] |> pass()
     end
-    
+
     test "`except` fills in foreign keys" do
-      stub(neighborhood: %{een(:inserted) => %{a: 5, species_id: 12},
-                           een(:bovine)   => %{              id: 12}})
+      [after: %{een(:bovine) => %{id: 12}},
 
-      input = %{
-                            actual:          %{a: 5, species_id: 12},
-                                     opts: [except: [species_id: id_of(:bovine)]]
-      }
-
-      input |> pass()
+       compare: %{a: 5, species_id: 12        },
+       against: %{a: 5, species_id: "replaced"},
+               except: [species_id: id_of(:bovine)]] |> pass()
     end
   end
 
   describe "global 'usually_ignore'" do
     test "has an effect" do
       stub(usually_ignore: [:lock_value])
-      stub(neighborhood: %{een(:inserted) => %{a: 5, lock_value: 8}})
-      run_args = %{opts: [], actual:         %{a: 5, lock_value: 9}}
-      run_args |> pass()
+
+      [compare: %{a: 5,      lock_value: 9},
+       against: %{a: 5,      lock_value: 8}] |> pass()
     end
 
-    test "merges" do
+    test "merges `:usually_ignore` with a specific `:ignoring`" do
       stub(usually_ignore: [:lock_value])
-      stub(neighborhood: %{een(:inserted) => %{a: 5, lock_value: 8}})
-      run_args = %{opts: [ignoring: [:a]],
-                   actual:                   %{a: 8, lock_value: 9}}
-      run_args |> pass()
+
+      [compare:  %{a: 8,     lock_value: 9},
+       against:  %{a: 5,     lock_value: 8},
+       ignoring: [:a]] |> pass()
     end
 
     test "`comparing` cancels out the effects of the global `usually_ignore`" do
       stub(usually_ignore: [:a])
-      stub(neighborhood: %{een(:inserted) => %{a: 5, lock_value: 8}})
-      run_args = %{opts: [comparing: [:a]],
-                   actual:                   %{a: 5, lock_value: 9}}
-      run_args |> pass()
+
+             # although lock_value is wrong, it's not listed in `comparing`
+      [compare: %{a: 5, lock_value: 9},
+       against: %{a: 5, lock_value: 8},
+       comparing: [:a]] |> pass()
     end
-    
+
+    test "if a value is explicitly `except`ed, that overrides `usually_ignore`" do
+      stub(usually_ignore: [:a])
+
+      run_args = [compare: %{a: 5},
+                  against: %{a: 5},
+                  except: [a: "wrong"]]
+
+      assertion_fails(~r/Example `:example`/,
+        [message: ~r/`:a` has the wrong value/,
+         left:  5,
+         right: "wrong"],
+        fn ->
+          run(run_args)
+        end)
+    end
+
+
+    test "... but other values ae still ignored" do
+      stub(usually_ignore: [:a, :lock_value])
+
+      run_args = [compare: %{a: "right",       lock_value: 22},
+                  against: %{a: 5, lock_value: 10},
+                  except: [a: "right"]]
+      pass(run_args)
+    end      
   end
 
   describe "both `fields` and `fields_like` can be used" do
     setup do 
       stub(field_checks: [a: "right"])
-      stub(fields_like: Rnode.FieldsLike.new(een(:inserted), [comparing: [:b]]))
+      stub(fields_like: Rnode.FieldsLike.new(een(:reference_struct), [comparing: [:b]]))
       :ok
     end
 
 
     test "that `fields` can fail" do 
-      stub(neighborhood: %{een(:inserted) => %{a: "right", b: "right"}})
-      stub_history(updated_value:            %{a: "wrong", b: "right"})
+      stub(neighborhood: %{een(:reference_struct) => %{a: "right", b: "right"}})
+      stub_history(new_struct:            %{a: "wrong", b: "right"})
 
     assertion_fails(~r/Field `:a` has the wrong value/,
       [left:  "wrong",
        right: "right"],
         fn ->
-          Steps.field_checks(:running, :updated_value)
+          Steps.field_checks(:running, :new_struct)
         end)
     end
 
     test "that `fields_like` can fail" do 
-      stub(neighborhood: %{een(:inserted) => %{a: "right", b: "right"}})
-      stub_history(updated_value:            %{a: "right", b: "wrong"})
+      stub(neighborhood: %{een(:reference_struct) => %{a: "right", b: "right"}})
+      stub_history(new_struct:            %{a: "right", b: "wrong"})
 
       assertion_fails(~r/Assertion with == failed/,
       [left:  %{b: "wrong"},
        right: %{b: "right"}],
         fn ->
-          Steps.field_checks(:running, :updated_value)
+          Steps.field_checks(:running, :new_struct)
         end)
     end
   end
