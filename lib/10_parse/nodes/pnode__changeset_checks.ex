4cb2b1d90a106aca240e07a3ca5ca1b4ae524715
Brian Marick <marick@exampler.com>
2021-03-10 12:47:26 -0600

    upgrade changeset_checks

--- lib/10_parse/nodes/pnode__changeset_checks.ex
+++ lib/10_parse/nodes/pnode__changeset_checks.ex
@@ -4,10 +4,23 @@ defmodule EctoTestDSL.Parse.Pnode.ChangesetChecks do
   use T.Drink.Assertively
   alias Pnode.ChangesetChecks, as: CC
   
-  defstruct parsed: [], with_ensured_eens: [], eens: []
+  defstruct parsed: [], eens: []
 
   def parse(kws), do: new(kws)
-  def new(kws), do: %CC{parsed: kws}
+  def new(kws) do
+    %CC{parsed: kws,
+        eens: Enum.flat_map(kws, &top_level/1)}
+  end
+
+  defp top_level({_top_key, next_level}) when is_list(next_level),
+    do: Enum.flat_map(next_level, &lower_level/1)
+  defp top_level({_top_key, ~M(%FieldRef een)}), do: [een]
+  defp top_level({_top_key, _some_value      }), do: [   ]
+  
+  defp lower_level({_lower_key, ~M(%FieldRef een)}), do: [een]
+  defp lower_level({_lower_key, _value}),            do: [   ]
+  defp lower_level(~M(%FieldRef een)   ),            do: [een]
+  defp lower_level(_value              ),            do: [   ]
 
   defimpl Pnode.Mergeable, for: CC do
     def merge(%CC{parsed: earlier}, %CC{parsed: later}) do
@@ -20,24 +33,13 @@ defmodule EctoTestDSL.Parse.Pnode.ChangesetChecks do
     def eens(%{eens: eens}), do: eens
 
     def ensure_eens(node, _default_module) do
-      eens = Enum.flat_map(node.parsed, &top_level/1)
-      %{node | eens: eens, with_ensured_eens: node.parsed}
+      node
     end
-
-    defp top_level({_top_key, next_level}) when is_list(next_level),
-      do: Enum.flat_map(next_level, &lower_level/1)
-    defp top_level({_top_key, ~M(%FieldRef een)}), do: [een]
-    defp top_level({_top_key, _some_value      }), do: [   ]
-    
-    defp lower_level({_lower_key, ~M(%FieldRef een)}), do: [een]
-    defp lower_level({_lower_key, _value}),            do: [   ]
-    defp lower_level(~M(%FieldRef een)   ),            do: [een]
-    defp lower_level(_value              ),            do: [   ]
   end
 
   defimpl Pnode.Exportable, for: CC do
     def export(node) do
-      node.with_ensured_eens
+      node.parsed
     end
   end
 end
--- test/10_parse/35_nodes/pnode__changeset_checks_test.exs
+++ test/10_parse/35_nodes/pnode__changeset_checks_test.exs
@@ -4,26 +4,17 @@ defmodule Parse.Pnode.ChangesetChecksTest do
   use T.Parse.Exports
   alias Pnode.ChangesetChecks, as: CC
 
-  test "creation" do
-    actual = CC.parse(changed: [x: 5])
-    assert actual.parsed == [changed: [x: 5]]
-  end
-
-  test "merging" do
-    first = CC.parse(changed: [x: 5])
-    second = CC.parse(errors: [:x], changed: [y: 8])
-    actual = Pnode.Mergeable.merge(first, second)
-    assert actual.parsed == [changed: [x: 5], errors: [:x], changed: [y: 8]]
-  end
-
-  describe "een handling" do
+  describe "creation" do
     defp expect(checks, expected) do
       node = CC.parse(checks)
-      actual = Pnode.EENable.ensure_eens(node, :ignored)
-      assert actual.with_ensured_eens == node.parsed
-      assert Pnode.EENable.eens(actual) == expected
+      assert node.parsed == checks
+      assert Pnode.EENable.eens(node) == expected
     end
     
+    test "no eens" do
+      [changed: [x: 5]] |> expect([])
+    end
+
     test "makes no changes eens does nothing, since `id_of` produces true eens" do
       [changed: [x: 5], changed: [y: id_of(:example)]] |> expect([een(:example)])
     end
@@ -37,6 +28,13 @@ defmodule Parse.Pnode.ChangesetChecksTest do
       [changed: [:x, y: 5]] |> expect([])
       [changed: [id_of(:one), y: id_of(:two)]] |> expect([een(:one), een(:two)])
     end
+  end
+
+  test "merging" do
+    first = CC.parse(changed: [x: 5])
+    second = CC.parse(errors: [:x], changed: [y: 8])
+    actual = Pnode.Mergeable.merge(first, second)
+    assert actual.parsed == [changed: [x: 5], errors: [:x], changed: [y: 8]]
   end
 
   test "export" do         # also add some top-level tests for merging and eens
