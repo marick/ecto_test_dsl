c9e0cf2668a64e50dd3b6ccd11bcb9645897103d
Brian Marick <marick@exampler.com>
2021-03-13 16:19:38 -0600

    Flesh out `from` options

--- lib/00_nouns/struct_ref.ex
+++ lib/00_nouns/struct_ref.ex
@@ -1,25 +1,41 @@
 defmodule EctoTestDSL.Nouns.StructRef do
   use EctoTestDSL.Drink.Me
-  alias T.Parse.Pnode.Common.EENWithOpts
+  alias T.Parse.Pnode.Common.FromPairs
   alias T.Nouns
+  import ExUnit.Assertions
 
   @moduledoc """
   A reference to an entire example
   """
 
-  defstruct [:reference_een, :eens, :opts]
+  defstruct [:reference_een, :eens, :except, :ignoring, :only]
   
-  def new(een, opts) do 
-    EENWithOpts.parse(__MODULE__, een, opts)
+  def new(reference_een, opts) do 
+    except = Keyword.get(opts, :except, []) |> Enum.into(%{})
+    eens = [reference_een | FromPairs.extract_een_values(except)]
+    ignoring = Keyword.get(opts, :ignoring, [])
+    only = Keyword.get(opts, :only, [])
+
+    if not Enum.empty?(ignoring) && not Enum.empty?(only),
+    do: flunk("You can't use both `ignoring:` and `only:` when referring to another example")
+
+    ~M{%__MODULE__ reference_een, except, eens, ignoring, only}
   end
 
   defimpl Nouns.RefHolder, for: __MODULE__ do
     def eens(value), do: value.eens
 
     def dereference(ref, in: neighborhood) do
-      neighborhood
-      |> Neighborhood.fetch!(ref.reference_een, :params)
+      base = Neighborhood.fetch!(neighborhood, ref.reference_een, :params)
+      filtered = 
+        case {ref.only, ref.ignoring} do
+          {[  ], [    ]} -> base
+          {[  ], ignore} -> Map.drop(base, ignore)
+          {only, [    ]} -> Map.take(base, only)
+        end
+
+      Map.merge(filtered, 
+        Neighborhood.Expand.values(ref.except, with: neighborhood))
     end
   end
-
 end
--- test/00_nouns/struct_ref_test.exs
+++ test/00_nouns/struct_ref_test.exs
@@ -3,25 +3,72 @@ defmodule Nouns.StructRefTest do
   alias T.Nouns.RefHolder
   use T.Parse.Exports
 
+
+  @een een(example_name: Examples)
+  @other_een een(:a)
+  @other_ref id_of(:a)
+
+  def create(opts), do: StructRef.new(@een, opts)
+
   test "creation" do
-    ref_een = een(example_name: Examples)
+    create([])
+    |> assert_fields(reference_een: @een,
+                     eens: [@een],
+                     except: %{}, ignoring: [], only: [])
 
-    ref = StructRef.new(ref_een, except: [b_id: id_of(:b)])
+    create([except: [a_id: @other_ref]])
+    |> assert_fields(reference_een: @een,
+                     eens: [@een, @other_een],
+                     except: %{a_id: @other_ref},
+                     ignoring: [],
+                     only: [])
 
-    assert_fields(ref, reference_een: ref_een, opts: [except: [b_id: id_of(:b)]])
-    assert_good_enough(RefHolder.eens(ref), in_any_order([ref_een, een(:b)]))
+    create([except: [a_id: @other_ref], ignoring: [:m]])
+    |> assert_fields(reference_een: @een,
+                     eens: [@een, @other_een],
+                     except: %{a_id: @other_ref},
+                     ignoring: [:m],
+                     only: [])
+
+    create([only: [:m]])
+    |> assert_fields(reference_een: @een,
+                     eens: [@een],
+                     except: %{},
+                     ignoring: [],
+                     only: [:m])
+
+    assertion_fails(~r/both `ignoring:` and `only:`/,
+      fn ->
+        create([only: [:m], ignoring: [:m]])
+      end)
   end
 
+  @neighborhood %{
+    @een => Neighborhood.Value.params(%{a: 5, other_id: "start"}),
+    @other_een => Neighborhood.Value.inserted(%{id: "override"})
+  }
+
+  defp deref(opts), do: create(opts) |> RefHolder.dereference(in: @neighborhood)
 
   describe "dereference" do
     test "simple case" do
-      neighborhood = %{
-        een(:een) => Neighborhood.Value.params(%{a: 5})
-      }
-      ref = StructRef.new(een(:een), [])
+      deref([])
+      |> assert_equal(%{a: 5, other_id: "start"})
+    end
 
-      RefHolder.dereference(ref, in: neighborhood)
-      |> assert_equal(%{a: 5})
+    test "with except" do
+      deref(except: [other_id: @other_ref])
+      |> assert_equal(%{a: 5, other_id: "override"})
+    end
+
+    test "ignoring" do
+      deref(ignoring: [:a])
+      |> assert_equal(%{other_id: "start"})
+    end
+    
+    test "only" do
+      deref(only: [:other_id])
+      |> assert_equal(%{other_id: "start"})
     end
   end
 end 
