08d6d6b8ba1f76fced6124459a38a8612607136b
Brian Marick <marick@exampler.com>
2021-03-26 18:01:15 -0500

    Update mockery_extras

--- test/support/running_stubs.ex
+++ test/support/running_stubs.ex
@@ -5,13 +5,13 @@ defmodule EctoTestDSL.RunningStubs do
 
   defmacro stub(kws) do
     for {key, val} <- kws do
-      Given.expand(RunningExample, [{key, 1}], [:running], val)
+      Given.expand(RunningExample, [{key, 1}], [:running], return: val)
     end
   end
 
   defmacro stub_history(kws) do
     for {key, val} <- kws do
-      Given.expand(RunningExample, [step_value!: 2], [:running, key], val)
+      Given.expand(RunningExample, [step_value!: 2], [:running, key], return: val)
     end
   end
 end
